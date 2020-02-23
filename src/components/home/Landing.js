36174d19b95991ce48a3a77e1d3b496efe7daddc
harsha Murupudi <itsmeeeharsha@gmail.com>
2020-02-23 19:26:25 +0530

    Make landing responsive

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -4,8 +4,8 @@ import LandingImg from '../../images/landing-women.webp';
 function Landing() {
   return (
     <section id="landing" className="d-flex flex-column justify-content-around">
-      <div className="row">
-        <div className="col-md-5 d-flex flex-column justify-content-center align-items-start text-left">
+      <div className="order-0 row mb-5 mb-md-0">
+        <div className="order-1 order-md-0 col-12 col-md-5 d-flex flex-column justify-content-center align-items-start text-left">
           <p>
             <span>We procure handwoven and handmade sarees, dress materials,fabric and other items direct from the Indian Artisans.</span>
           </p>
@@ -13,19 +13,19 @@ function Landing() {
             <a href="/"><span>Shop Now</span></a>
           </div>
         </div>
-        <img className="col-md-7" src={LandingImg} alt="women weaving" />
+        <img className="order-0 order-md-1 col-12 col-md-7" src={LandingImg} alt="women weaving" />
 
       </div>
 
-      <div id="landing-form" className="row d-flex">
-        <div className="col-md-5"><h1>Handpicked Handlooms</h1></div>
-        <form className="col-md-7 d-flex" action="post">
-          <div class="order-2 col-md-8 floating-form">
+      <div id="landing-form" className="row d-flex order-1">
+        <div className="col-12 col-md-5"><h1>Handpicked Handlooms</h1></div>
+        <form className="col-12 col-md-7 d-block d-md-flex" action="post">
+          <div class="order-1 d-inline col-12 col-md-8 floating-form">
             <input class="floating-input" type="text" placeholder=" " id="email" name="email" aria-describedby="email" />
             <span class="highlight"></span>
             <label for="email">Email Address</label>
           </div>
-          <div className="order-2 col-md-4 shop-btn">
+          <div className="order-2 col-12 col-md-4 shop-btn">
             <a href="/"><span>Sign up</span></a>
           </div>
         </form>
